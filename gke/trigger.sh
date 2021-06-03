c533c2e890bd04525a49a19c7191e4a16c19a5a4
KyleDJ <65569030+kdj-dev@users.noreply.github.com>
2021-06-03 15:16:08 +0100

    Update trigger.sh

--- gke/trigger.sh
+++ gke/trigger.sh
@@ -4,7 +4,7 @@ gcloud config set project $PROJECT_ID
 triggers=$(gcloud beta builds triggers list | grep build-gke-trigger)
 if [ -z "$triggers" ]; then
 gcloud beta builds triggers create github \
---repo-name="iac-orchestration" \
+--repo-name="iac-orchestration-dev-proj" \
 --repo-owner="CPT-CE" \
 --name="build-gke-trigger" \
 --build-config="gke/cloudbuild.yaml" \
