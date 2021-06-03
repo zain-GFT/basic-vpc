a36917a0e182949cbb5616862879c565757ecc66
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2021-06-03 14:43:18 +0100

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -8,5 +8,11 @@ module "vpc" {
 
   subnets = var.subnets
 
-  secondary_ranges = var.secondary_ranges
+  secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = var.subnet.gke_secondary_ip_range.pods.name
+                ip_cidr_range = var.subnet.gke_secondary_ip_range.pods.range
+            },
+        ]
 }
