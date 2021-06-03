bc53466ed6f57df985c0421d3ce7faabe0bbb017
KyleDJ <65569030+kdj-dev@users.noreply.github.com>
2021-06-03 18:43:45 +0100

    Update gke.tf

--- gke/gke.tf
+++ gke/gke.tf
@@ -10,6 +10,9 @@ module "gke" {
   ip_range_services          = var.subnet.gke_secondary_ip_range.services.name
 
   enable_pod_security_policy = false
+  
+  default_max_pods_per_node  = 29
+  master_ipv4_cidr_block.     = var.subnet.gke_master_range
 
   node_pools = [
       {
